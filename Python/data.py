332fd0512293dfa65ccf7ff0e9245fd02b918884
Sergio <scobos@students.mchenry.edu>
2026-08-26 11:26:58 -0500

    Python/data.py

---
+++ Python/data.py
@@ -0,0 +1,15 @@
+# allowed strings
+print("Double quotes")
+print("single quotes")
+
+print('print "Double quotes" ')
+print('print "single quotes" ')
+print("")
+#  👻 BOO lean boolean
+tired = True
+awake = True
+coffee = False
+
+print(coffee)
+if tired:
+    print("I need coffee")
