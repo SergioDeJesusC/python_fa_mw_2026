d616f52d5ed942e9b565eef7c28e2548ad0b4a45
SergioDeJesusC <scobos@students.mchenry.edu>
2026-08-26 10:10:03 -0500

    Add files via upload

---
+++ Python/joke.py
@@ -0,0 +1,5 @@
+#  Assignment information goes at the top
+"""
+  How many men do you need to properly cook a good meal❔
+  None they probably ask there wives for two portions💀
+"""
---
+++ Python/madlibs.py
@@ -0,0 +1,6 @@
+name = input("Please enter a person's name: ")
+animal = input("Please enter a type of animal: ")
+color = input("Please enter a color: ")
+
+
+# 
