8b96b8b70d71258851c75db37ad509db3b6675de
SergioDeJesusC <scobos@students.mchenry.edu>
2026-09-14 11:50:08 -0500

    Update validation.py

---
+++ validation.py
@@ -0,0 +1,20 @@
+# Name check
+
+# Rule - can't be empty
+# should have first letter capitalized (we handle)
+
+try:
+    fname = ""
+    while not fname:
+        fname = input("Please enter your first name:")
+        fname = fname.strip()
+        fname = input("Please enter your last name:")
+        fname = fname.strip()
+
+
+
+
+
+
+
+
