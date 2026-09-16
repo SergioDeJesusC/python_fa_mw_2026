b5476d10fe7f35f9b54fa4267b8615b9baf13873
SergioDeJesusC <scobos@students.mchenry.edu>
2026-09-16 11:35:31 -0500

    Add menu for balance management
    Initial implementation of a menu for balance management with options for checking balance, depositing, and withdrawing.

---
+++ Menu.py
@@ -0,0 +1,21 @@
+balance = 1000.00
+choice = 1
+while choice > 0 and choice < 4:
+    print(f" 1. Balance")
+    print(f" 2. Deposit")
+    print(f" 3. Withdraw")
+    print(f" 4. Exit")
+    choice = int(input("Please enter the number of your selection:  "))
+
+    match choice:
+        case 1:
+            print("Balance")
+            print(f"Your balance is:  ${balance:.2f}")
+        case 2:
+            print("Deposit")
+            print(f"How much you wnat to deposit")
+        case 3:
+            print("Withdraw")
+            print(f"How much you wnat to Withdraw")
+        case 4:
+            print("Good bye!")
