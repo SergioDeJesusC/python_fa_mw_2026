b4a645f6dc2121efc5bbf6d31029e2364b3c2818
Sergio <scobos@students.mchenry.edu>
2026-09-21 10:59:34 -0500

    Fix menu option display and improve case handling in user selection

---
+++ list notes.py
@@ -0,0 +1,40 @@
+""""
+list, arraway, vectors, tuples
+
+python - lists are muteable, tuplus are immutable 
+others languages have arrays and vectors
+
+"""
+
+# create list
+
+names = ["Doc", "Grumpy", "Happy", "sleepy", "Bashful", "Sneezy", "Dopey"]
+
+# for dwarf in dwarves:
+#     print(dwarf)
+
+#    print(dwarves[2])
+colle_classes = [
+    "introduction to psychology",
+    "Calculus I",
+    "World History",
+    "General Chemistry",
+    "English Composition",
+    "Microeconomics",
+    "Introduction to Programming",
+    "Spanic II",
+    "Organic Chemistry",
+    "Statistics",
+]
+# add to the end
+college_classes.append("web Desing")
+
+for item in college_classes:
+    print(item)
+
+v_c = dwarves[2:6]
+
+v_c.append("saracastic")
+
+for dwarf in dwarves:
+    print(dwarf)
--- menu notes.py
+++ menu notes.py
@@ -15,7 +15,7 @@ print(f"1.  Create a new contact")
 print(f"2.  Search contact")
 print(f"3.  Update contact")
 print(f"4.  Delete contact")
-print(f"5.  ")
+print(f"5.  Quit")
 
 # get user choise
 choice: 1
@@ -25,12 +25,23 @@ while choice > 0 and choice < 4:
 
         case 1:
             print("Create")
+            continue
         case 2:
             print("Seach")  # Read
-        #Delete contact
+            # Delete contact
+            continue
+
         case 3:
             print("Update")
+            continue
+
         case 4:
             print("Delete")
+            continue
+
         case 5:
             print("Good bye!")
+            continue
+
+        case _:
+            print("Good ")
