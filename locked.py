5489a732bb436a76c189504f8b2754f5b39be417
Sergio <scobos@students.mchenry.edu>
2026-09-23 11:15:10 -0500

    Remove merge conflict markers and clean up locked.py

--- locked.py
+++ locked.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 constants vs tuples
 """
@@ -17,23 +16,3 @@ print(SCHOOL_NAME)
 # SCHOOL_NAME.pop()
 
 print(len(SCHOOL_NAME))
-=======
-"""
-constants vs tuples
-"""
-
-SCHOOL_NAME = ("McHenry County College")
-
-print(SCHOOL_NAME)
-
-# replaces does not modify (same as strings)
-SCHOOL_NAME = ("Elgin Community College")
-
-print(SCHOOL_NAME)
-
-
-# not allowed
-# SCHOOL_NAME.pop()
-
-print(len(SCHOOL_NAME))
->>>>>>> 42624caa95bdab418756f332e0592b0ed67d1200
