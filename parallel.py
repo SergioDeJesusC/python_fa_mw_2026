5ac5e574145c367cd81f8c5bde8bd01dbac38c8c
SergioDeJesusC <scobos@students.mchenry.edu>
2026-09-23 11:11:52 -0500

    Add files via upload

---
+++ parallel.py
@@ -0,0 +1,28 @@
+"""
+Constants and Tuples
+parallel arrays
+
+"""
+
+
+states = tuple[str, ...] = (
+    
+)
+
+
+state_capital = tuple[str, ...] = (
+
+)
+
+state_capital_biard = (
+
+)
+
+# my_state = states.sort()
+
+# for index in range(0, len(states)):
+#     print(f"{states[index]},  {state_capital[index]}")
+
+
+print(f"{states_and_capitals[12], [0]}")
+print(f"{states_and_capitals[12], [1]}")
