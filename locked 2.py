8207c9f47fbf8c9528335c57a0b3cbe3dc4ea539
Sergio <scobos@students.mchenry.edu>
2026-09-23 10:41:27 -0500

    Add initial implementation of constants and tuples in locked.py

---
+++ locked 2.py
@@ -0,0 +1,18 @@
+"""
+constants vs tuples
+"""
+
+SCHOOL_NAME = ("McHenry County College")
+
+print(SCHOOL_NAME)
+
+# replaces does not modify (same as strings)
+SCHOOL_NAME = ("Elgin Community College")
+
+print(SCHOOL_NAME)
+
+
+# not allowed
+# SCHOOL_NAME.pop()
+
+print(len(SCHOOL_NAME))
